152c82a1b4e57cd8decc2e6468fe7e1dacdc061a
conor.budge <conor.budge@concordusa.com>
2017-05-12 08:31:22 -0500

    HN home page connecting to their Firebase API, handled in Angular with Observables

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import {Component, ViewEncapsulation} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  encapsulation: ViewEncapsulation.None,
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'app works!';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,32 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { StoriesComponent } from './stories/stories.component';
+import { FooterComponent } from './footer/footer.component';
+import { ItemComponent } from './item/item.component';
+import {HackernewsApiService} from "./hackernews-api.service";
+import {MomentModule} from "angular2-moment";
+import {DomainPipe} from "./domain.pipe";
+import {BusyModule} from "angular2-busy";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    StoriesComponent,
+    FooterComponent,
+    ItemComponent,
+    DomainPipe
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    MomentModule,
+    BusyModule
   ],
-  providers: [],
+  providers: [HackernewsApiService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
